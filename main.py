5b6bd2a191713f12d6f3ac1c8476a605086785c0
bartoszkonarski <barto.konarski@gmail.com>
2021-04-24 20:39:03 +0200

    deployment branch added

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ class GithubUser(Resource):
 api.add_resource(GithubUser,"/api/<string:username>")
 
 if __name__ == "__main__":
-    app.run(debug=False,port=10716)
+    app.run(debug=False,port=33507)
